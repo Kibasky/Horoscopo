54adfb426821afb1b920a7b702ec3aa055966895
Damivk <damianpemo@gmail.com>
2024-10-29 20:58:12 +0100

    zodiak xml files

---
+++ app/src/main/java/com/example/horoscopo/data/HoroscopeProvider.kt
@@ -0,0 +1,18 @@
+package com.example.horoscopo.data
+
+import com.example.horoscopo.R
+
+class HoroscopeProvider {
+    companion object {
+
+        private val horoscopeList: List<Horoscope> = listOf(
+            Horoscope( "Aries", R.string.horoscope_name_aries, R.string.horoscope_date_aries, )
+
+
+
+        )
+
+
+
+    }
+}
